baf3df19ff61d9b3d6afb103e86f9bc581f22f1a
Kieran <98961254+XLV150@users.noreply.github.com>
2022-04-25 18:31:06 +0200

    Update and rename Madlibs.py to Anagrams.py

--- Madlibs.py
+++
@@ -1,7 +0,0 @@
-adjective1 = input("Enter a adjective: ")
-place1 = input("Enter a place: ")
-colour1 = input("Enter a colour: ")
-name1 = input("Enter a name: ")
-day1 = input("Enter a day: ")
-
-print("Last night I was {}, I came home from a {}. The walls at {}, were {}. The guy I met there had an odd name, it was {}. This all happened on {}.".format(adjective1, place1, place1, colour1, name1, day1))
